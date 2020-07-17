d1186adb5c1638cc73b55e08d897fe2124b4f426
RafaelAlvesSantos <rafaelalvesmgs@gmail.com>
2020-07-17 17:27:58 -0300

    finished Navbar component with its child components

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About this App</h1>
+      <p className="my-1">
+        This is a fullstack app for easily checking your friends and families'
+        wishlists.
+      </p>
+      <p className="bg-dark p">Version: 0.1</p>
+    </div>
+  );
+};
+
+export default About;
